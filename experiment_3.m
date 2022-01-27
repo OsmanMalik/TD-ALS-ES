aa4d4acad6b36934d8ee2bfe75c6f6391f5702eb
Osman Asif Malik <osman.malik.87@gmail.com>
2022-01-27 15:10:53 -0700

    Update experiment script to allow for multiple trials

--- experiment_3.m
+++ experiment_3.m
@@ -9,99 +9,114 @@
 vec = @(x) x(:);
 [X, class_array] = data_loader('coil-100');
 
-method = "tr_als";
+method = "cp_als_es";
+no_trials = 5;
+ACC = zeros(no_trials, 1);
+ER = zeros(no_trials, 1);
+TIME = zeros(no_trials, 1);
 
 sz = size(X);
 ranks = [5,5,5,5];
 CP_rank = 25;
 tol = 0;
-maxiters = 20;
-exp3_tic = tic;
-switch method
-    case "cp_als"
-        CPD = cp_als(tensor(X), CP_rank, 'tol', tol, 'maxiters', maxiters);
-        factor_mats = CPD.U;
-        lambda = CPD.lambda;
-    case "cp_als_basic"
-        factor_mats = cp_als_basic(X, CP_rank, 'maxiters', maxiters);
-        lambda = ones(CP_rank,1);
-    case "cpd_als" % Tensorlab
-        options.Algorithm = @cpd_als;
-        options.MaxIter = maxiters;
-        factor_mats = cpd(X, CP_rank, options);
-        lambda = ones(CP_rank,1);
-    case "cpd_minf" % Tensorlab
-        options.Algorithm = @cpd_minf;
-        factor_mats = cpd(X, CP_rank, options);
-        lambda = ones(CP_rank,1);
-    case "cpd_nls" % Tensorlab
-        options.Algorithm = @cpd_nls;
-        options.MaxIter = maxiters;
-        factor_mats = cpd(X, CP_rank, options);
-        lambda = ones(CP_rank,1);
-    case "cpd3_sd" % Tensorlab -- only 3-way tensors
-        options.Algorithm = @cpd3_sd;
-        options.MaxIter = maxiters;
-        factor_mats = cpd(X, CP_rank, options);
-        lambda = ones(CP_rank,1);
-    case "cpd3_sgsd" % Tensorlab -- only 3-way tensors
-        options.Algorithm = @cpd3_sgsd;
-        options.MaxIter = maxiters;
-        factor_mats = cpd(X, CP_rank, options);
-        lambda = ones(CP_rank,1);
-    case "cpd_rbs" % Tensorlab
-        options.Algorithm = @cpd_rbs;
-        options.MaxIter = maxiters;
-        factor_mats = cpd(X, CP_rank, options);
-        lambda = ones(CP_rank,1);
-    case "cp_arls_lev"
-        J = 1000;
-        factor_mats = cp_arls_lev(X, CP_rank, J, 'maxiters', maxiters);
-        lambda = ones(CP_rank,1);
-    case "cp_als_es"
-        J1 = 1000;
-        J2 = 1000;        
-        factor_mats = cp_als_es(X, CP_rank, J1, J2, 'maxiters', maxiters, 'permute_for_speed', true);
-        lambda = ones(CP_rank,1);
-    case "tr_als"
-        cores = tr_als(X, ranks, 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'verbose', true);
-    case "rtr_als"
-        K = [20 20 3 200];
-        cores = rtr_als(X, ranks, K, 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'verbose', true);
-    case "tr_als_sampled"
-        J = 1000;
-        cores = tr_als_sampled(X, ranks, J*ones(size(sz)), 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'resample', true, 'verbose', true);
-    case "tr_svd"
-        cores = TRdecomp_ranks(X, ranks);
-    case "tr_svd_rand"
-        oversamp = 10;
-        cores = tr_svd_rand(X, ranks, oversamp);
-    case "tr_als_es"
-        J1 = 1000;
-        J2 = 1000;
-        cores = tr_als_es(X, ranks, J1, J2*ones(size(sz)), 'tol', tol, 'permute_for_speed', true, 'conv_crit', 'relative error', 'maxiters', maxiters, 'verbose', true);
-    otherwise
-        error('Invalid method.')
-end
-exp3_toc = toc(exp3_tic);
+maxiters = 40;
+for tr = 1:no_trials
+    exp3_tic = tic;
+    switch method
+        case "cp_als"
+            CPD = cp_als(tensor(X), CP_rank, 'tol', tol, 'maxiters', maxiters);
+            factor_mats = CPD.U;
+            lambda = CPD.lambda;
+        case "cp_als_basic"
+            factor_mats = cp_als_basic(X, CP_rank, 'maxiters', maxiters);
+            lambda = ones(CP_rank,1);
+        case "cpd_als" % Tensorlab
+            options.Algorithm = @cpd_als;
+            options.MaxIter = maxiters;
+            factor_mats = cpd(X, CP_rank, options);
+            lambda = ones(CP_rank,1);
+        case "cpd_minf" % Tensorlab
+            options.Algorithm = @cpd_minf;
+            factor_mats = cpd(X, CP_rank, options);
+            lambda = ones(CP_rank,1);
+        case "cpd_nls" % Tensorlab
+            options.Algorithm = @cpd_nls;
+            options.MaxIter = maxiters;
+            factor_mats = cpd(X, CP_rank, options);
+            lambda = ones(CP_rank,1);
+        case "cpd3_sd" % Tensorlab -- only 3-way tensors
+            options.Algorithm = @cpd3_sd;
+            options.MaxIter = maxiters;
+            factor_mats = cpd(X, CP_rank, options);
+            lambda = ones(CP_rank,1);
+        case "cpd3_sgsd" % Tensorlab -- only 3-way tensors
+            options.Algorithm = @cpd3_sgsd;
+            options.MaxIter = maxiters;
+            factor_mats = cpd(X, CP_rank, options);
+            lambda = ones(CP_rank,1);
+        case "cpd_rbs" % Tensorlab
+            options.Algorithm = @cpd_rbs;
+            options.MaxIter = maxiters;
+            factor_mats = cpd(X, CP_rank, options);
+            lambda = ones(CP_rank,1);
+        case "cp_arls_lev"
+            J = 2e+3*[1 1 1 1];
+            factor_mats = cp_arls_lev(X, CP_rank, J, 'maxiters', maxiters);
+            lambda = ones(CP_rank,1);
+        case "cp_als_es"
+            J1 = [10000 10000 20000 10000];
+            J2 = 1000*[1 1 1 1];        
+            factor_mats = cp_als_es(X, CP_rank, J1, J2, 'maxiters', maxiters, 'permute_for_speed', true);
+            lambda = ones(CP_rank,1);
+        case "tr_als"
+            cores = tr_als(X, ranks, 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'verbose', true);
+        case "rtr_als"
+            K = [20 20 3 200];
+            cores = rtr_als(X, ranks, K, 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'verbose', true);
+        case "tr_als_sampled"
+            J = 1000;
+            cores = tr_als_sampled(X, ranks, J*ones(size(sz)), 'tol', tol, 'conv_crit', 'relative error', 'maxiters', maxiters, 'resample', true, 'verbose', true);
+        case "tr_svd"
+            cores = TRdecomp_ranks(X, ranks);
+        case "tr_svd_rand"
+            oversamp = 10;
+            cores = tr_svd_rand(X, ranks, oversamp);
+        case "tr_als_es"
+            J1 = 10000;
+            J2 = 1000;
+            cores = tr_als_es(X, ranks, J1, J2*ones(size(sz)), 'tol', tol, 'permute_for_speed', true, 'conv_crit', 'relative error', 'maxiters', maxiters, 'verbose', true);
+        otherwise
+            error('Invalid method.')
+    end
+    exp3_toc = toc(exp3_tic);
+    TIME(tr) = exp3_toc;
+    
+    % The if statement below makes sure that the 4th core/factor matrix is
+    % reshaped appropriately.
+    if contains(method, "tr") % TR method
+        feat_mat = reshape(permute(cores{end}, [2 1 3]), 7200, ranks(end-1)*ranks(end));
+        Y = cores_2_tensor(cores);
+    else % CP method
+        feat_mat = factor_mats{end};
+        Y = double(tensor(ktensor(lambda, factor_mats)));    
+    end
 
-% The if statement below makes sure that the 4th core/factor matrix is
-% reshaped appropriately.
-if contains(method, "tr") % TR method
-    feat_mat = reshape(permute(cores{end}, [2 1 3]), 7200, ranks(end-1)*ranks(end));
-    Y = cores_2_tensor(cores);
-else % CP method
-    feat_mat = factor_mats{end};
-    Y = double(tensor(ktensor(lambda, factor_mats)));    
+    Mdl = fitcknn(feat_mat, class_array, 'numneighbors', 1);
+    cvmodel = crossval(Mdl);
+    loss = kfoldLoss(cvmodel, 'lossfun', 'classiferror');
+    accuracy = 1 - loss;
+    ACC(tr) = accuracy;
+    er = norm(vec(X - Y))/norm(vec(Y));
+    ER(tr) = er;
+
+    fprintf('Accuracy is %.4f\n', accuracy);
+    fprintf('Error is %.4f\n', er);
+    fprintf('Decomposition time was %.4f\n', exp3_toc);
 end
 
-Mdl = fitcknn(feat_mat, class_array, 'numneighbors', 1);
-cvmodel = crossval(Mdl);
-loss = kfoldLoss(cvmodel, 'lossfun', 'classiferror');
-accuracy = 1 - loss;
-fprintf('Accuracy is %.4f\n', accuracy);
-fprintf('Error is %.4f\n', norm(vec(X - Y))/norm(vec(Y)))
-fprintf('Decomposition time was %.4f\n', exp3_toc);
+fprintf('Mean accuracy is %.4f\n', mean(ACC));
+fprintf('Mean error is %.4f\n', mean(ER));
+fprintf('Mean decomposition time was %.4f\n', mean(TIME));
 %{
 %save("experiment_3_" + method + "_results.mat", 'accuracy', 'exp3_toc');
 %}
